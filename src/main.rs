5bc1a10f716ba2c85bf93e42706fc9e44e348d19
Zhang Yanpo <drdr.xp@gmail.com>
2025-03-29 17:22:47 +0800

    feat: update dep

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::io::{self, Read};
 
-fn main() -> Result<(), String> {
+fn main() -> anyhow::Result<()> {
 
     let mut md = String::new();
-    io::stdin().read_to_string(&mut md).map_err(|e| e.to_string())?;
+    io::stdin().read_to_string(&mut md)?;
 
     println!(
         "{}",
         markdown::to_html_with_options(
             &md,
             &markdown::Options::gfm()
-        )?
+        ).map_err(|e| anyhow::anyhow!("{}", e.to_string()))?
     );
 
     Ok(())
